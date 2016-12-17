b64a80b0a7ec2767010df492e8ffb83b24aca849
Anthony Graddy <anthony@locationq.com>
2016-12-17 07:15:17 +0000

    Working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,31 @@
 var tap = require('agraddy.test.tap')(__filename);
+var response = require('agraddy.test.res');
 
-var mod = require('../');
+var htm = require('../');
+var routes = {};
+var res = response();
+var res2 = response();
 
-tap.assert.equal(mod.main('result'), 'result', 'Should be equal.');
+process.chdir('test');
+
+(function() {
+	htm(routes, '/about');
+
+	res.on('finish', function() {
+		tap.assert.equal(res._body, 'about\n', 'Should parse the data.');
+	});
+
+	routes['/about'](null, {}, res);
+})();
+
+
+(function() {
+	htm(routes, '/take/two');
+	res2.on('finish', function() {
+		tap.assert.equal(res2._body, 'take_two\n', 'Should get file with two slashes.');
+	});
+
+	routes['/take/two'](null, {}, res2);
+})();
 
 
